076ac86640d6601601b4cf99f031b8ea0dbab3fc
Connor Mason <connor_a_mason@apple.com>
2015-08-12 13:34:46 -0700

    Made JS syntactically more efficient

--- controlPanel.js
+++ controlPanel.js
@@ -4,6 +4,7 @@ var automationRef   = ref.child("homeAutomation");
 // ********************** Make values reload when database updated ************
 
 function loadData() {
+	// Load modules
 	automationRef.child("relayModules").once("value", function(modulesSnapshot) {
 		// Check to make sure relayModules reference exists
 		if (modulesSnapshot.val() === null) {
@@ -25,19 +26,7 @@ function chooseButtonColor(value) {
 	}
 }
 
-function createModule(moduleData) {
-	// Content variables
-	var moduleNum = moduleData.key();
-	var moduleName = moduleData.val().location;
-	var button1Text = moduleData.val().relays[1].name;
-	var button2Text = moduleData.val().relays[2].name;
-	var button3Text = moduleData.val().relays[3].name;
-	var button4Text = moduleData.val().relays[4].name;
-	var button1Value = moduleData.val().relays[1].value;
-	var button2Value = moduleData.val().relays[2].value;
-	var button3Value = moduleData.val().relays[3].value;
-	var button4Value = moduleData.val().relays[4].value;
-
+function generatePanel(panelTitleIn) {
 	// Create col-md-4 div
 	var colElement = document.createElement("div");
 	colElement.className = "col-md-4";
@@ -55,7 +44,7 @@ function createModule(moduleData) {
 
 	// Create panel title
 	var panelTitle = document.createElement("h3");
-	var titleText = document.createTextNode("Module " + moduleNum + ": " + moduleName);
+	var titleText = document.createTextNode(panelTitleIn);
 	panelTitle.className = "panel-title";
 	panelTitle.appendChild(titleText);
 	panelHeading.appendChild(panelTitle);
@@ -65,51 +54,38 @@ function createModule(moduleData) {
 	panelBody.className = "panel-body";
 	panel.appendChild(panelBody);
 
+	return panelBody;
+}
+
+function createModule(moduleData) {
+	// Content variables
+	var moduleNum = moduleData.key();
+	var moduleName = moduleData.val().location;
+	var buttonText = [moduleData.val().relays[1].name, moduleData.val().relays[2].name, 
+					moduleData.val().relays[3].name, moduleData.val().relays[4].name];
+	var buttonValue = [moduleData.val().relays[1].value, moduleData.val().relays[2].value, 
+					moduleData.val().relays[3].value, moduleData.val().relays[4].value];
+
+	// Generate module panel 
+	var panelBody = generatePanel("Module " + moduleNum + ": " + moduleName);
+
 	// Create button toolbar
 	var buttonToolbar = document.createElement("div");
 	buttonToolbar.className = "btn-toolbar";
 	panelBody.appendChild(buttonToolbar);
 
 	// Create buttons
-	var button1 = document.createElement("button");
-	var button1TextElement = document.createTextNode(button1Text);
-	button1.type = "button";
-	button1.style.margin = "2px";
-	button1.className = chooseButtonColor(button1Value);
-	button1.id = "module" + moduleNum + "button1";
-	button1.addEventListener("click", function() { toggleRelay(moduleNum, 1); });
-	button1.appendChild(button1TextElement);
-	buttonToolbar.appendChild(button1);
-
-	var button2 = document.createElement("button");
-	var button2TextElement = document.createTextNode(button2Text);
-	button2.type = "button";
-	button2.style.margin = "2px";
-	button2.className = chooseButtonColor(button2Value);
-	button2.id = "module" + moduleNum + "button2";
-	button2.addEventListener("click", function() { toggleRelay(moduleNum, 2); });
-	button2.appendChild(button2TextElement);
-	buttonToolbar.appendChild(button2);
-
-	var button3 = document.createElement("button");
-	var button3TextElement = document.createTextNode(button3Text);
-	button3.type = "button";
-	button3.style.margin = "2px";
-	button3.className = chooseButtonColor(button3Value);
-	button3.id = "module" + moduleNum + "button3";
-	button3.addEventListener("click", function() { toggleRelay(moduleNum, 3); });
-	button3.appendChild(button3TextElement);
-	buttonToolbar.appendChild(button3);
-
-	var button4 = document.createElement("button");
-	var button4TextElement = document.createTextNode(button4Text);
-	button4.type = "button";
-	button4.style.margin = "2px";
-	button4.className = chooseButtonColor(button4Value);
-	button4.id = "module" + moduleNum + "button4";
-	button4.addEventListener("click", function() { toggleRelay(moduleNum, 4); });
-	button4.appendChild(button4TextElement);
-	buttonToolbar.appendChild(button4);
+	for (var i = 0; i < 4; i++) {
+		var button = document.createElement("button");
+		var buttonTextElement = document.createTextNode(buttonText[i]);
+		button.type = "button";
+		button.style.margin = "2px";
+		button.className = chooseButtonColor(buttonValue[i]);
+		button.id = "module" + moduleNum + "button" + String(i + 1);
+		button.addEventListener("click", function() { toggleRelay(moduleNum, i + 1); });
+		button.appendChild(buttonTextElement);
+		buttonToolbar.appendChild(button);
+	}
 }
 
 function toggleRelay(moduleNum, buttonNum) {
