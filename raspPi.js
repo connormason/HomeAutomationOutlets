0dfca7d2824aee7b8d7ce80730a8719679e00e9a
Connor Mason <connor_a_mason@apple.com>
2015-08-12 18:11:09 -0700

    Implemented relayModule watcher for Rasp Pi

---
+++ raspPi.js
@@ -0,0 +1,33 @@
+var SerialPort = require("serialport");
+var colors     = require("colors/safe");
+    Firebase   = require("firebase");
+
+// Configuration variables
+var verbose = true;
+
+// Firebase references
+var ref             = new Firebase('https://connormason.firebaseio.com');
+var automationRef   = ref.child("homeAutomation");
+var relayModulesRef = automationRef.child("relayModules");
+
+// ********************* Functions *********************
+function sendRelayData(module, data) {
+    if (verbose) { console.log(colors.green("Sending updated relay info for module " + module + " to Arduino...")); }
+}
+// ********************* /Functions ********************
+
+// If relay values change, act accordingly
+relayModulesRef.on("child_changed", function(modulesSnapshot) {
+// relayModulesRef.once("value", function(modulesSnapshot) {
+    if (verbose) { console.log("relayModules child changed"); }
+
+    // Compress data into smaller JSON object and send to Arduino
+    var modulePacket = {};
+    var relays = modulesSnapshot.val().relays;
+    for (var i = 1; i < relays.length; i++) {
+        modulePacket[i] = relays[i].value;
+    }
+    sendRelayData(modulesSnapshot.val().moduleNum, modulePacket);
+});
+
+// If mode values change, act accordingly
