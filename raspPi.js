c44c78fb864229683a9183c1864800381890440a
Connor Mason <connor_a_mason@apple.com>
2015-08-20 15:06:08 -0700

    Testing write over serial to Arduino functionality

--- raspPi.js
+++ raspPi.js
@@ -11,31 +11,35 @@ var automationRef   = ref.child("homeAutomation");
 var modesRef        = automationRef.child("modes");
 var relayModulesRef = automationRef.child("relayModules");
 
-// ********************* Functions *********************
-function sendRelayData(module, data) {
-    if (verbose) { console.log(colors.green("Sending updated relay info for module " + module + " to Arduino...")); }
-}
-
-function sendModeData(data) {
-    
-}
-// ********************* /Functions ********************
-
-// If relay values change, act accordingly
-relayModulesRef.on("child_changed", function(modulesSnapshot) {
-// relayModulesRef.once("value", function(modulesSnapshot) {
-    if (verbose) { console.log("relayModules child changed"); }
-
-    // Compress data into smaller JSON object and send to Arduino
-    var modulePacket = {};
-    var relays = modulesSnapshot.val().relays;
-    for (var i = 1; i < relays.length; i++) {
-        modulePacket[i] = relays[i].value;
-    }
-    sendRelayData(modulesSnapshot.val().moduleNum, modulePacket);
+// Serial port intialization
+var serialPort = new SerialPort.SerialPort("/dev/tty.usbmodem1421", {
+    baudrate: 9600,
+    parser: SerialPort.parsers.readline("\n")
 });
 
-// If mode values change, act accordingly
-modesRef.on("child_changed", function(modesSnapshot) {
-    if (verbose) { console.log("modes child changed"); }
+serialPort.on("open", function() {
+    if (verbose) { console.log("Serial port opened"); }
+
+    // If relay values change, act accordingly
+    relayModulesRef.on("child_changed", function(modulesSnapshot) {
+        // if (verbose) { console.log("relayModules child changed"); }
+
+        // Compress data into smaller JSON object and send to Arduino
+        var modulePacket = {};
+        var relays = modulesSnapshot.val().relays;
+        for (var i = 1; i < relays.length; i++) {
+            modulePacket[i] = relays[i].value;
+        }
+        // sendRelayData(modulesSnapshot.val().moduleNum, modulePacket);
+        serialPort.write(modulesSnapshot.val().moduleNum, function(err, results) {
+            // if (verbose) { console.log(colors.green("Sending updated relay info for module " + module + " to Arduino...")); }
+            if (err != null) { console.log(colors.red("  Write errors: " + err)); }
+        });
+    });
+
+    // If mode values change, act accordingly
+    modesRef.on("child_changed", function(modesSnapshot) {
+        if (verbose) { console.log("modes child changed"); }
+    });
 });
+
